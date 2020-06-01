b4d03090e2a3dbd282c28a43a4b6eaebefbac78b
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-01 21:52:29 +0530

    Day 1,1/6/2020

---
+++ Week 1/Invert Binary Tree.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    return &TreeNode{
+        root.Val,
+        invertTree(root.Right),
+        invertTree(root.Left),
+    }
+}
