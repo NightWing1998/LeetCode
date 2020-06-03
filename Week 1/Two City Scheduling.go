1da4ffe134d205311a4213bc274950a9e1f119cb
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-04 02:11:43 +0530

    Day 3, 4/6/2020

---
+++ Week 1/Two City Scheduling.go
@@ -0,0 +1,20 @@
+import "sort"
+
+func twoCitySchedCost(costs [][]int) int {
+	// Greedy move: sort by diff with 0 first having the least cost
+	sort.Slice(costs, func(i, j int) bool {
+		return (costs[i][0] - costs[i][1]) < (costs[j][0] - costs[j][1])
+	})
+	sum := 0
+	n := len(costs)
+	// First all cities with least cost for a
+	for i := 0; i < n/2; i++ {
+		sum += costs[i][0]
+	}
+	for j := n / 2; j < n; j++ {
+		sum += costs[j][1]
+	}
+	return sum
+}
+
+// Help from redit was taken
