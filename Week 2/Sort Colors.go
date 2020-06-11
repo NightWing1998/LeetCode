dba3cb6bb6af661414b6c742c0d291feab38be64
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-11 17:21:02 +0530

    Day 11, 11/06/2020

---
+++ Week 2/Sort Colors.go
@@ -0,0 +1,24 @@
+func sortColors(nums []int) {
+	var count = make(map[int]int)
+	for _, color := range nums {
+		_, exists := count[color]
+		if exists {
+			count[color]++
+		} else {
+			count[color] = 1
+		}
+	}
+	redCount, redExists := count[0]
+	whiteCount, whiteExists := count[1]
+	blueCount, blueExists := count[2]
+	start := 0
+	for ; redExists && start < redCount && start < len(nums); start++ {
+		nums[start] = 0
+	}
+	for ; whiteExists && start < redCount+whiteCount && start < len(nums); start++ {
+		nums[start] = 1
+	}
+	for ; blueExists && start < redCount+whiteCount+blueCount && start < len(nums); start++ {
+		nums[start] = 2
+	}
+}
