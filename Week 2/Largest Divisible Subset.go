ade17b8bc88fefe788561f6824966532ff5a3b3e
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-13 15:37:02 +0530

    Day 13, 13/06/2020

---
+++ Week 2/Largest Divisible Subset.go
@@ -0,0 +1,35 @@
+import "sort"
+
+func largestDivisibleSubset(nums []int) []int {
+	if len(nums) == 0 {
+		return []int{}
+	}
+	sort.SliceStable(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	div := make([]int, len(nums))
+	prev := make([]int, len(nums))
+	maxDivIndex := 0
+	for i := 0; i < len(nums); i++ {
+		prev[i] = -1
+		div[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i]%nums[j] == 0 && div[i] < div[j]+1 {
+				div[i] = div[j] + 1
+				prev[i] = j
+			}
+		}
+		if div[maxDivIndex] < div[i] {
+			maxDivIndex = i
+		}
+	}
+
+	res := make([]int, div[maxDivIndex])
+
+	iter := maxDivIndex
+	for i := div[maxDivIndex] - 1; iter >= 0; i, iter = i-1, prev[iter] {
+		res[i] = nums[iter]
+	}
+
+	return res
+}
