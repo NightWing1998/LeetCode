aae4fdf177e04851c2b6659a62d0b1eda06a92e7
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-21 11:49:40 +0530

    Day 20, 21/06/2020

---
+++ Week 3/Permutation Sequence.go
@@ -0,0 +1,24 @@
+import (
+	"strings"
+)
+
+func getPermutation(n int, k int) string {
+	nums := make([]int, n)
+	var table = make([]int, 9)
+	table[0] = 1
+	nums[0] = 1
+	for i := 1; i < n; i++ {
+		nums[i] = i + 1
+		table[i] = i * table[i-1]
+	}
+	k--
+	var ans strings.Builder
+	for i := n - 1; i >= 0; i-- {
+		j := k / table[i]
+		ans.WriteByte(byte(nums[j]) + '0')
+		k -= j * table[i]
+
+		nums = append(nums[:j], nums[j+1:]...)
+	}
+	return ans.String()
+}
