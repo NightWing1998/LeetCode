3961fa0c7ae0042c00362466a9ae571a456f2012
NightWing1998 <dhruvil.s@somaiya.edu>
2020-06-27 23:01:50 +0530

    Day 27,06/27/2020

---
+++ Week 4/Perfect Squares.go
@@ -0,0 +1,24 @@
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func numSquares(n int) int {
+	if n < 4 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[0], dp[1], dp[2], dp[3] = 0, 1, 2, 3
+	for i := 4; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			if dp[i] != 0 {
+				dp[i] = min(dp[i], dp[i-j*j]+1)
+			} else {
+				dp[i] = dp[i-j*j] + 1
+			}
+		}
+	}
+	return dp[n]
+}
---
+++ git-fast.sh
@@ -0,0 +1,2 @@
+git add . && git commit -m "Day $(date +"%d"),$(date +"%m/%d/%Y")"
+git push
