78a7c41c9f4ceccfd17d5de7903cc1ddf1314e8c
NightWing1998 <dhruvil.s@somaiya.edu>
2020-07-01 11:46:25 +0530

    Day 30, 30/06/2020

---
+++ June/Week 5/Word Search II.go
@@ -0,0 +1,58 @@
+type Trie struct {
+	word     string
+	children [26]*Trie
+}
+
+// take arr of string as parameter and generate a Trie out of it
+func GenerateTrie(words []string) *Trie {
+	var root Trie = Trie{}
+	for _, word := range words {
+		current := &root
+		for _, ch := range word {
+			if current.children[ch-'a'] == nil {
+				current.children[ch-'a'] = &Trie{} // if alpha does not have a node the gen one
+			}
+			current = current.children[ch-'a']
+		}
+		current.word = word // assign word to last alphabet of word
+	}
+	return &root
+}
+
+func findWords(board [][]byte, words []string) []string {
+	result := make([]string, 0, len(words))
+	root := GenerateTrie(words)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			dfs(board, i, j, root, &result)
+		}
+	}
+	return result
+}
+
+func dfs(board [][]byte, i, j int, root *Trie, result *[]string) {
+	ch := board[i][j]
+	if ch == '*' || root.children[ch-'a'] == nil { // * indicates visited cell
+		return
+	}
+	temp := root.children[ch-'a'] // pick up the node coresponding to alpha
+	if len(temp.word) != 0 {      // if word  != "" the found match append to result
+		*result = append(*result, temp.word)
+		temp.word = "" // delete word so that it will be added again in result
+	}
+
+	board[i][j] = '*' // cell visited
+	if i > 0 {
+		dfs(board, i-1, j, temp, result) // up
+	}
+	if j > 0 {
+		dfs(board, i, j-1, temp, result) // left
+	}
+	if i < len(board)-1 {
+		dfs(board, i+1, j, temp, result) // down
+	}
+	if j < len(board[i])-1 {
+		dfs(board, i, j+1, temp, result) // right
+	}
+	board[i][j] = ch // backtracking
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LeetCodeJune
-All LeetCode June challenge problems
+# Leet Code Monthly Challenges
+
+<p>
+	This repository contains solutions to the Leet Code monthly challenges in golang.
+	All the Solutions are ordered by Months, weeks and problem name.
+	Each problem contains runtime and ranking commented out in the end of the file
+</p>
